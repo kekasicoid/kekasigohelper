588960ade72b25d26d30e4acf2429a6b0fe3ff4b
Kekasi Gen <kekasi.co.id@gmail.com>
2022-02-02 11:04:10 +0700

    add RandStringBytesMaskImprSrcUnsafe

--- mylibrary.go
+++ mylibrary.go
@@ -1,9 +1,43 @@
 package kekasigohelper
 
 import (
+	"math/rand"
 	"net/http"
 	"strings"
+	"time"
+	"unsafe"
 )
+
+// START Generate Random String
+// https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+const letterBytes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+func RandStringBytesMaskImprSrcUnsafe(n int) string {
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+// END Generate Random String
 
 func InArray(str string, list []string) bool {
 	str = strings.ToLower(str)
