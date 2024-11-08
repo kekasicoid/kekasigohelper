295e28a95baeae14f8c6138c8ba2e615fb8dd7bc
Arditya Kekasi <arditya.konsultan@work.bri.co.id>
2024-11-08 09:35:41 +0700

    add lib : anytostring

---
+++ anytostring.go
@@ -0,0 +1,20 @@
+package kekasigohelper
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func anyToString(value interface{}) string {
+	switch v := value.(type) {
+	case string:
+		return v
+	case int:
+		return strconv.Itoa(v)
+	case float64:
+		return strconv.FormatFloat(v, 'f', -1, 64)
+	// Add cases for other types as needed
+	default:
+		return fmt.Sprintf("%v", v) // Fallback to fmt for other types
+	}
+}
