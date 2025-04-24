f7edfdb0c06e8e8501c2cb2d0e4e836860b02f0e
Arditya Kekasi <arditya.konsultan@work.bri.co.id>
2025-04-24 09:17:10 +0700

    Add dateDiffDay

--- time_formater.go
+++ time_formater.go
@@ -25,16 +25,16 @@ func DateDiffMidnight(now time.Time) time.Duration {
 }
 
 func DateDiffDay(startDate string, endDate string) (days int, err error) {
-    // Parse the dates
-    layout := "2006-01-02"
-    start, err := time.Parse(layout, startDate)
-    if err != nil {
-        return 0, fmt.Errorf("error parsing start date: " + err.Error())
-    }
-    end, err := time.Parse(layout, endDate)
-    if err != nil {
-        return 0, fmt.Errorf("error parsing end date: " + err.Error())
-    }
-    // Calculate the difference in days
-    return int(end.Sub(start).Hours() / 24), nil
+	// Parse the dates
+	layout := "2006-01-02"
+	start, err := time.Parse(layout, startDate)
+	if err != nil {
+		return 0, fmt.Errorf("error parsing start date: " + err.Error())
+	}
+	end, err := time.Parse(layout, endDate)
+	if err != nil {
+		return 0, fmt.Errorf("error parsing end date: " + err.Error())
+	}
+	// Calculate the difference in days
+	return int(end.Sub(start).Hours() / 24), nil
 }
