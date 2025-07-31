5642f8de6e436682e7764cd81d1149133f7a1105
Arditya Kekasi <arditya.konsultan@work.bri.co.id>
2025-07-31 10:09:05 +0700

    [ADD] Database : Checksum table

---
+++ checksum/checksum.go
@@ -0,0 +1,77 @@
+package checksum
+
+import (
+	"crypto/md5"
+	"database/sql"
+	"encoding/hex"
+	"fmt"
+	"log"
+)
+
+func GetAllTables(db *sql.DB) ([]string, error) {
+	rows, err := db.Query("SHOW TABLES")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var tables []string
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			return nil, err
+		}
+		tables = append(tables, name)
+	}
+	return tables, nil
+}
+
+func ChecksumTable(db *sql.DB, table string) (string, error) {
+	query := fmt.Sprintf("SELECT * FROM `%s`", table)
+	rows, err := db.Query(query)
+	if err != nil {
+		return "", err
+	}
+	defer rows.Close()
+
+	columns, _ := rows.Columns()
+	values := make([]sql.RawBytes, len(columns))
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	hasher := md5.New()
+
+	for rows.Next() {
+		if err := rows.Scan(scanArgs...); err != nil {
+			return "", err
+		}
+		for _, val := range values {
+			hasher.Write(val)
+		}
+	}
+
+	return hex.EncodeToString(hasher.Sum(nil)), nil
+}
+
+func ChecksumAllTables(db *sql.DB) map[string]string {
+	results := make(map[string]string)
+
+	tables, err := GetAllTables(db)
+	if err != nil {
+		log.Println("Error getting tables:", err)
+		return results
+	}
+
+	for _, table := range tables {
+		sum, err := ChecksumTable(db, table)
+		if err != nil {
+			log.Printf("Error checksumming table %s: %v\n", table, err)
+			continue
+		}
+		results[table] = sum
+	}
+
+	return results
+}
