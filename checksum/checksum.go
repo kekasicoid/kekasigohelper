5a1bfa360b50e866aa7cfd126cfba5076bea77e2
Arditya Kekasi <arditya.konsultan@work.bri.co.id>
2025-08-04 09:16:05 +0700

    [ADD] Checksum table for mysql

--- checksum/checksum.go
+++ checksum/checksum.go
@@ -84,3 +84,53 @@ func ChecksumAllTables(db *sql.DB) map[string]string {
 
 	return results
 }
+
+func ChecksumAllTablesMySQL(db *sql.DB) map[string]string {
+	results := make(map[string]string)
+
+	tables, err := GetAllTables(db)
+	if err != nil {
+		log.Println("Error getting tables : ", err)
+		return results
+	}
+
+	for _, table := range tables {
+		sum, err := ChecksumTable(db, table)
+		if err != nil {
+			log.Printf("Error checksumming table %s: %v\n", table, err)
+			continue
+		}
+		results[table] = sum
+	}
+
+	return results
+}
+
+func ChecksumTableMySQL(db *sql.DB, table string) (string, error) {
+	start := time.Now()
+	query := fmt.Sprintf("CHECKSUM TABLE `%s`", table)
+	log.Println("Executing checksum for ", table, " : ", query)
+	rows, err := db.Query(query)
+	if err != nil {
+		return "", err
+	}
+	defer rows.Close()
+
+	var sum string
+	if rows.Next() {
+		var tblName string
+		var checksum sql.NullInt64
+		if err := rows.Scan(&tblName, &checksum); err != nil {
+			return "", err
+		}
+		if checksum.Valid {
+			sum = fmt.Sprintf("%d", checksum.Int64)
+		} else {
+			sum = ""
+		}
+	}
+
+	duration := time.Since(start)
+	log.Println("Checksum for table", table, "is", sum, " | ", duration)
+	return sum, nil
+}
